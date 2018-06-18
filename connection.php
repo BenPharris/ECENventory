17c63c7422fce52e158bfb78c359dfb6a9f2fc34
grantmatheny <grant.matheny@colorado.edu>
2018-06-18 13:40:14 -0600

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = new mysqli("localhost", "user_inventory", "E7RFjEwFmLJegGI1", "custom_inventory");
-
-if ($link->connect_error){
-	die("Error: Could not connect. " . $link->connect_error);
-}
-?>
